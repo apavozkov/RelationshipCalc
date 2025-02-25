a5848f0f89bccdc7a7d8380711992885a6b8089f
Sergey <sergey@Sergeys-MacBook-Pro.local>
2025-02-25 07:46:51 +0530

    feat: additive formulas

--- api/app/controllers/api/v1/formulas_controller.rb
+++ api/app/controllers/api/v1/formulas_controller.rb
@@ -4,13 +4,27 @@ class Api::V1::FormulasController < ApplicationController
     render json: formulas, status: 200
   end
 
+  # def create
+  #   formula = Formula.new(
+  #     name: params[:name],
+  #     formula: params[:formula]
+  #   )
+  #   if formula.save
+  #     render json: formula, status: 200 else
+  #     render json: {error: "Error creating formula."} end
+  # end
+
   def create
+    base_formula = Formula.where(["name = '%s'", params[:sub_formula]]).pluck(:formula).first 
+    base_formula +=  params[:operation]
+    base_formula +=  params[:additive_formula]
     formula = Formula.new(
       name: params[:name],
-      formula: params[:formula]
+      formula: base_formula
     )
     if formula.save
       render json: formula, status: 200 else
       render json: {error: "Error creating formula."} end
   end
+
 end
